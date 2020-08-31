4bf2a2866d6a7866e4e416d75eba5499296f3284
nirmash <nirmsk@hotmail.com>
2020-08-31 14:43:36 -0700

    test post processing

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+project_name = '{{ cookiecutter.project_name }}'
+function_name = '{{ cookiecutter.function_name }}'
+aws_region = '{{ cookiecutter.aws_region }}'
+
+echo $project_name
+echo $function_name
+echo $aws_region
+
 mkdir .github
 mkdir .github/workflows
 curl https://config-helpers.s3-us-west-2.amazonaws.com/deploy.yml > .github/workflows/deploy.yml
