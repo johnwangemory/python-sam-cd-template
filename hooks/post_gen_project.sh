536988e301996fdb5196285f8d37da8b1624a062
nirmash <nirmsk@hotmail.com>
2020-08-31 18:11:43 -0700

    fix cmd.sh

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir .github
 mkdir .github/workflows
-curl https://config-helpers.s3-us-west-2.amazonaws.com/deploy.yml > .github/workflows/deploy.yml
+curl https://config-helpers.s3-us-west-2.amazonaws.com/deploy.yml?versionId=null > .github/workflows/deploy.yml
+aws s3 mb s3://bucket-{{ cookiecutter.project_name }} --region "{{ cookiecutter.aws_region }}"
 echo "Template ready"
