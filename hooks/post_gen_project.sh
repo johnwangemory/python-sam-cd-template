7afb128e8ff37281c133ab30cd1abcfd082ea838
nirmash <nirmsk@hotmail.com>
2020-08-31 18:31:48 -0700

    remove the bucket creation

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,5 +2,5 @@
 mkdir .github
 mkdir .github/workflows
 curl https://config-helpers.s3-us-west-2.amazonaws.com/deploy.yml?versionId=null > .github/workflows/deploy.yml
-aws s3 mb s3://bucket-{{ cookiecutter.project_name }} --region "{{ cookiecutter.aws_region }}"
+cd {{ cookiecutter.project_name }}
 echo "Template ready"
