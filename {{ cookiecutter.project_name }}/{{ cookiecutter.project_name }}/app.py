7fc9d92bed7e5e8e911e97f355b78523792ef26c
nirmash <nirmsk@hotmail.com>
2020-09-01 08:16:30 -0700

    fix app.py

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/app.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/app.py
@@ -1,5 +1,5 @@
 
-def lambda_handler(event):
+def lambda_handler(event, context):
     message = 'Hello {} {}!'.format(event['obj_name'], 
                                     event['body'])  
     return { 
