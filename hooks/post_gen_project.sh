bc743fa4058ada65d6e9e0b311fc9b310cc63c0c
nirmash <nirmsk@hotmail.com>
2020-09-01 15:02:03 -0700

    make sure bucket name is lower case

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,6 +2,6 @@
 mkdir .github
 mkdir .github/workflows
 curl https://config-helpers.s3-us-west-2.amazonaws.com/deploy.yml?versionId=null > .github/workflows/deploy.yml
-bucket="`{{ cookiecutter.project_name }} | tr 'A-Z' 'a-z'`"
+bucket="`echo {{ cookiecutter.project_name }} | tr 'A-Z' 'a-z'`"
 aws s3 mb s3://bucket-$bucket --region "{{ cookiecutter.aws_region }}"
 echo "Template ready"
