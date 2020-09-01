a325d768682ac1a12a53c2cea32e4d7ea4a47de5
nirmash <nirmsk@hotmail.com>
2020-09-01 10:19:18 -0700

    try to add bucket creation to template download

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,5 +2,5 @@
 mkdir .github
 mkdir .github/workflows
 curl https://config-helpers.s3-us-west-2.amazonaws.com/deploy.yml?versionId=null > .github/workflows/deploy.yml
-cd {{ cookiecutter.project_name }}
+aws s3 mb s3://bucket-{{ cookiecutter.project_name }} --region "{{ cookiecutter.aws_region }}"
 echo "Template ready"
